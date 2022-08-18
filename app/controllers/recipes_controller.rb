2d6b1c12407cbd519a66a9dd90e32f95d25674cf
Brian Kubisiak <brian@kubisiak.com>
2022-08-18 12:33:02 -0700

    recipe: Implement basic CRU interface
    Using the 'resources :recipes' route in rails, add a new CRU (Delete
    is not yet implemented) interface for creating, reading, and updating
    recipes in the database. This is mostly copied from rail's "Getting
    Started" tutorial.
    
    The HTML is fairly barebones right now.
    
    Signed-off-by: Brian Kubisiak <brian@kubisiak.com>

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+# Map recipe requests to views generated from the database
+class RecipesController < ApplicationController
+  def index
+    @recipes = Recipe.all
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+
+    if @recipe.save
+      redirect_to @recipe
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    if @recipe.update(recipe_params)
+      redirect_to @recipe
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, :body, :ingredients)
+  end
+end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,7 @@
+# frozen_string_literal: true
+
 class Recipe < ApplicationRecord
+  validates :title, presence: true
+  validates :ingredients, presence: true
+  validates :body, presence: true
 end
