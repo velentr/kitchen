2ee2967fbd39e404eefc9301212540b03b6e92b0
Brian Kubisiak <brian@kubisiak.com>
2022-08-23 13:51:40 -0700

    views: recipes: Render a "recipe history" grid
    On the recipe's page, render a left-side grid using color to indicate
    how many times the recipe has been cooked in the month for the past 36
    months.
    
    Signed-off-by: Brian Kubisiak <brian@kubisiak.com>

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,9 +1,26 @@
 # frozen_string_literal: true
 
+require 'date'
+
+def months_between(start, finish)
+  (finish.year * 12 + finish.month) - (start.year * 12 + start.month)
+end
+
+# Model for recipes in the database
 class Recipe < ApplicationRecord
   has_many :cooks
 
   validates :title, presence: true
   validates :ingredients, presence: true
   validates :body, presence: true
+
+  def count_cooks
+    num_cells = 36
+    all_cooks = cooks.where('created_at > ?', num_cells.months.ago).group_by do |cook|
+      months_between(DateTime.now, cook.created_at)
+    end
+    (0..num_cells - 1).map do |i|
+      all_cooks[i].nil? ? 0 : all_cooks[i].length
+    end
+  end
 end
