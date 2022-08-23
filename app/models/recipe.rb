dbfd7962e12832bf54563f09693982e03b7e6206
Brian Kubisiak <brian@kubisiak.com>
2022-08-23 12:02:31 -0700

    models: recipe: Add has_many cooks relation
    This is necessary to finish binding the relationship between recipes
    and cooks.
    
    Signed-off-by: Brian Kubisiak <brian@kubisiak.com>

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Recipe < ApplicationRecord
+  has_many :cooks
+
   validates :title, presence: true
   validates :ingredients, presence: true
   validates :body, presence: true
