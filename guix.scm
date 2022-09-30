dabfdae6e5ba9c4592a0db032be5ac839c1a16c2
Brian Kubisiak <brian@kubisiak.com>
2022-09-30 14:36:01 -0700

    guix: Add initial package revision
    This builds and installs the package as a gem. The gem doesn't yet
    work due to the tmp and log directories getting located in APP_PATH; I
    still have to come up with a workaround for this.
    
    This is also renamed to guix.scm so it will work with a bare 'guix
    shell' command (no need to point it at the file).
    
    Signed-off-by: Brian Kubisiak <brian@kubisiak.com>

---
+++ guix.scm
@@ -0,0 +1,25 @@
+(use-modules (gnu packages rails)
+             (gnu packages ruby)
+             (guix build-system ruby)
+             (guix gexp)
+             (guix git)
+             ((guix licenses) #:prefix license:)
+             (guix packages))
+
+(package
+ (name "kitchen")
+ (version "0")
+ (source (git-checkout (url (dirname (current-filename)))))
+ (build-system ruby-build-system)
+ (arguments
+  (list #:tests? #f))  ;; I haven't written any real tests yet
+ (propagated-inputs
+  (list ruby-2.7 ruby-byebug ruby-listen ruby-kramdown ruby-rails
+        ruby-sprockets-rails ruby-sqlite3 ruby-tzinfo-data))
+ (native-inputs (list ruby-solargraph))  ;; for LSP
+ (synopsis "Personal recipe tracker")
+ (description "Kitchen is a personal recipe/cooking tracker that aims to be a
+self-hosted way of collecting your favorite recipes and how often you cook
+them.")
+ (home-page "https://github.com/velentr/kitchen")
+ (license license:expat))
